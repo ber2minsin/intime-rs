f6974e167e935ba942876973858c2adbd5b68572
ber2minsin <ber2minsin@gmail.com>
2026-05-14 00:00:00 +0300

    feat: Add storage configuration management

---
+++ crates/intime-storage/src/config.rs
@@ -0,0 +1,8 @@
+use serde::Serialize;
+
+
+#[derive(Clone, Serialize, Debug)]
+pub struct StorageConfig {
+    pub database_file: String, // TODO convert to option
+    // folder_path: Path, // TODO in the future
+}
