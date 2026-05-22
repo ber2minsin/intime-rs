90a52658165cc4eb4c7e0fbbc81d23bda0beb741
ber2minsin <ber2minsin@gmail.com>
2026-05-22 05:00:00 +0300

    config: Add Tauri configuration and capabilities

---
+++ crates/intime-app/src-tauri/src/error.rs
@@ -0,0 +1,13 @@
+use serde::Serialize;
+
+#[derive(thiserror::Error, Debug, Serialize)]
+pub enum AppError {
+    #[error("Internal error: {0}")]
+    // This allows the ? operator to convert anyhow::Error automatically
+    Anyhow(#[from] #[serde(serialize_with = "serialize_anyhow")] anyhow::Error),
+}
+
+fn serialize_anyhow<S>(err: &anyhow::Error, serializer: S) -> Result<S::Ok, S::Error>
+where S: serde::Serializer {
+    serializer.serialize_str(&format!("{:#}", err))
+}
---
+++ crates/intime-app/src-tauri/src/lib.rs
@@ -0,0 +1,64 @@
+use std::{env};
+use tokio::sync::Mutex;
+
+use intime_ai::{embedding::{EmbeddingClient, start_piped_server}, models::EmbeddingRequest};
+use intime_storage::{
+    config::StorageConfig,
+    storage::{Storage},
+};
+use tauri::{Manager as _};
+
+use crate::error::AppError;
+mod error;
+
+struct AppState {
+    embedding_server: Mutex<EmbeddingClient>,
+    storage: Storage
+}
+
+#[tauri::command]
+async fn search_embedding(query: &str, state: tauri::State<'_, AppState>) -> Result<String, AppError> {
+
+    let embedding_request = EmbeddingRequest::Text {
+        text: query.to_string(),
+    };
+    let mut embedding_server = state.embedding_server.lock().await;
+
+    let embedding_response = embedding_server.make_request(embedding_request).await?;
+    let found = state.storage
+        .embedding_repository
+        .search_embedding(embedding_response.embedding)
+        .await
+        .unwrap();
+    // let related_event_ids = found.iter().map(|embedding| storage.event_repository.get_event(embedding.event_id))
+
+    Ok(format!("{:#?}", found))
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|app| {
+
+            dotenv::dotenv().expect("Failed to load dotenv");
+            let embedding_server = Mutex::new(start_piped_server()?);
+            let storage_config = StorageConfig {
+                database_file: env::var("DATABASE_FILE").expect("DATABASE_FILE must be set"),
+            };
+
+            println!("{:?}", storage_config);
+            let storage = tauri::async_runtime::block_on(Storage::connect(&storage_config))
+                .expect("Can not connect to storage");
+
+            app.manage(AppState {
+                embedding_server,
+                storage
+            });
+
+            Ok(())
+        })
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![search_embedding])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
